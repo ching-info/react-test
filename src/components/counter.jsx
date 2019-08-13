797f3313d8d4c6dd80326673ac4cd1e588cd1b4d
ching-info <ching.lynard@infomaticscorp.com>
2019-08-13 12:48:45 -0700

    lesson one

---
+++ src/components/counter.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"]
+  };
+
+  constructor() {
+    super();
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <h3>No Tags</h3>;
+
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+
+  handleIncrement(product) {
+    console.log("Incremented", product);
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncrement({ id: 1 })}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+
+        {this.state.tags.length === 0 && "Please create a new tag"}
+        {this.renderTags()}
+      </React.Fragment>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
